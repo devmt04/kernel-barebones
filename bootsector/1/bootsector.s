e88a58bf94a0dce04bc1bdce75aaf43aaa8ef221
devmt04 <mohitdeoli004@gmail.com>
2023-10-28 17:56:52 +0530

    Revert "Restructured the directories"
    This reverts commit 6fe0c37355418d9c6e7910215ceab4a2224f0c34.

---
+++ bootsector/1/bootsector.s
@@ -0,0 +1,19 @@
+/*
+as bootsector.s -o bootsector.o
+ld --oformat binary bootsector.o -o bootsector.bin
+
+*/
+
+.code16
+
+label: 
+	/*As computer will run that code first which it encountered in bootsector
+	and this code leads to a infinite loop, so when the system boots up
+	with this boot sector, system will just go in a infinite loop*/
+	jmp label
+
+.fill 510-(. - label),1,0 	#upto here our 510 bytes of boot sector got covered
+
+.word 0xaa55 #boot signature(of 2 bytes). Boot signature must lies at 510 and 511 byte of bootsector
+
+#now here the 1st sector of disk got consumed completely
